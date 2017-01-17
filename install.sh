9a4705e5a69964985f53e65705000a5ee76d58eb
naoya3e <naoya3e@gmail.com>
2017-01-17 15:56:25 +0900

    [add] インストール用のシェルスクリプトを作成

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+ln -is $HOME/.dotfiles/gitconfig $HOME/.gitconfig
+ln -is $HOME/.dotfiles/nvimrc $HOME/.nvimrc
+ln -is $HOME/.dotfiles/zpreztorc $HOME/.zpreztorc
+ln -is $HOME/.dotfiles/zshenv $HOME/.zshenv
+ln -is $HOME/.dotfiles/zshrc $HOME/.zshrc
