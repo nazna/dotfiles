6000d35164ad6bed067724f533e77497437a74bf
naoya3e <naoya3e@gmail.com>
2017-01-21 01:54:56 +0900

    [add] NeoVimの設定用のファイルはここなので、こちらにもリンクが必要だった

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 ln -is $HOME/.dotfiles/gitconfig $HOME/.gitconfig
+ln -is $HOME/.dotfiles/nvimrc $HOME/.config/nvim/init.vim
 ln -is $HOME/.dotfiles/nvimrc $HOME/.nvimrc
 ln -is $HOME/.dotfiles/zpreztorc $HOME/.zpreztorc
 ln -is $HOME/.dotfiles/zshenv $HOME/.zshenv
