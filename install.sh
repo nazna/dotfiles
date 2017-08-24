f32194bd28a8e5e8e8a55ced6028cee2e67d4a86
naoya3e <naoya3e@gmail.com>
2017-08-25 04:45:21 +0900

    update: xfce4 terminal用のテーマをコピーする処理を追加

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
+sudo cp $HOME/.dotfiles/hybrid.theme /usr/share/xfce4/terminal/colorschemes/
+
 ln -is $HOME/.dotfiles/gitconfig $HOME/.gitconfig
 ln -is $HOME/.dotfiles/nvimrc $HOME/.config/nvim/init.vim
 ln -is $HOME/.dotfiles/nvimrc $HOME/.nvimrc
-ln -is $HOME/.dotfiles/zpreztorc $HOME/.zpreztorc
 ln -is $HOME/.dotfiles/zshenv $HOME/.zshenv
 ln -is $HOME/.dotfiles/zshrc $HOME/.zshrc
