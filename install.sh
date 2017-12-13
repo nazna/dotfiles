8a24bf396315ec4e65f807f5562b22e5e97ef380
Naoya Yamashita <naoya3e@gmail.com>
2017-12-14 00:58:00 +0900

    update: gitconfigの調整を行ってinstall用のshell scriptを更新

--- install.sh
+++ install.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
 if [ "$(uname)" != 'Darwin' ]; then
-  sudo cp $HOME/.dotfiles/hybrid.theme /usr/share/xfce4/terminal/colorschemes/
-  sudo cp $HOME/.dotfiles/onedark.theme /usr/share/xfce4/terminal/colorschemes/
+  sudo cp $HOME/.dotfiles/themes/hybrid.theme /usr/share/xfce4/terminal/colorschemes/
+  sudo cp $HOME/.dotfiles/themes/onedark.theme /usr/share/xfce4/terminal/colorschemes/
 fi
 
 ln -is $HOME/.dotfiles/gitconfig $HOME/.gitconfig
+ln -is $HOME/.dotfiles/globalignore $HOME/.globalignore
 ln -is $HOME/.dotfiles/nvimrc $HOME/.config/nvim/init.vim
 ln -is $HOME/.dotfiles/nvimrc $HOME/.nvimrc
 ln -is $HOME/.dotfiles/zshenv $HOME/.zshenv
