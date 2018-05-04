e2c37d42396a0dff0a9961d3a9bec975056f85f5
Naoya Yamashita <naoya3e@gmail.com>
2018-05-05 00:39:17 +0900

    fix: ghqでdotfilesも管理するようにしたのでsymboricのオリジナルの場所の記載を修正

--- install.sh
+++ install.sh
@@ -5,10 +5,10 @@ if [ "$(uname)" != 'Darwin' ]; then
   sudo cp $HOME/.dotfiles/themes/onedark.theme /usr/share/xfce4/terminal/colorschemes/
 fi
 
-ln -is $HOME/.dotfiles/gitconfig $HOME/.gitconfig
-ln -is $HOME/.dotfiles/globalignore $HOME/.globalignore
-ln -is $HOME/.dotfiles/latexmkrc $HOME/.latexmkrc
-ln -is $HOME/.dotfiles/nvimrc $HOME/.config/nvim/init.vim
-ln -is $HOME/.dotfiles/nvimrc $HOME/.nvimrc
-ln -is $HOME/.dotfiles/zshenv $HOME/.zshenv
-ln -is $HOME/.dotfiles/zshrc $HOME/.zshrc
+ln -nfs $HOME/.ghq/github.com/naoya3e/.dotfiles/gitconfig $HOME/.gitconfig
+ln -nfs $HOME/.ghq/github.com/naoya3e/.dotfiles/globalignore $HOME/.globalignore
+ln -nfs $HOME/.ghq/github.com/naoya3e/.dotfiles/latexmkrc $HOME/.latexmkrc
+ln -nfs $HOME/.ghq/github.com/naoya3e/.dotfiles/nvimrc $HOME/.config/nvim/init.vim
+ln -nfs $HOME/.ghq/github.com/naoya3e/.dotfiles/nvimrc $HOME/.nvimrc
+ln -nfs $HOME/.ghq/github.com/naoya3e/.dotfiles/zshenv $HOME/.zshenv
+ln -nfs $HOME/.ghq/github.com/naoya3e/.dotfiles/zshrc $HOME/.zshrc
