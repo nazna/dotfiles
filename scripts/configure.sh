59a9cb68a595a40d29c3bf246f7a10f64db44014
Naoya Yamashita <naoya3e@users.noreply.github.com>
2019-10-06 01:47:16 +0900

    chore: update settings trackpad and more

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -3,10 +3,16 @@ defaults write com.apple.screencapture disable-shadow -boolean true
 defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
 defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
 defaults write com.apple.CrashReporter DialogType -string "none"
+defaults write NSGlobalDomain AppleShowScrollBars -string "WhenScrolling"
+defaults write NSGlobalDomain AppleScrollerPagingBehavior -int 1
+
+echo "Configuring Desktop and Screensaver..."
+defaults write com.apple.screensaver idleTime -int 0
 
 echo "Configuring Dock..."
+defaults write com.apple.dock static-only -bool true
 defaults write com.apple.dock persistent-apps -array
-defaults write com.apple.dock tilesize -int 37
+defaults write com.apple.dock tilesize -int 40
 defaults write com.apple.dock autohide -bool true
 defaults write com.apple.dock autohide-delay -float 0
 defaults write com.apple.dock autohide-time-modifier -float 0
@@ -25,16 +31,22 @@ defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
 defaults write NSGlobalDomain WebAutomaticSpellingCorrectionEnabled -bool false
 defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
 defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
+defaults write NSGlobalDomain NSAutomaticCapitalizationEnabled -bool false
 
 echo "Configuring Trackpad..."
+defaults write com.apple.AppleMultitouchTrackpad Clicking -bool true
 defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
 defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
 defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
-
-echo "Configuring Software update..."
+defaults write com.apple.trackpad.forceClick -int 0
+defaults write com.apple.trackpad.scaling -int 2
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadThreeFingerDrag -int 1
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadThreeFingerHorizSwipeGesture -int 0
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadThreeFingerVertSwipeGesture -int 0
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadTwoFingerFromRightEdgeSwipeGesture -int 0
 
 echo "Configuring Date and Clock..."
-defaults write com.apple.menuextra.clock DateFormat -string "yyyy/MM/dd EEE HH:mm"
+defaults write com.apple.menuextra.clock DateFormat -string "MMM d (EEE) HH:mm"
 defaults write com.apple.menuextra.clock IsAnalog -bool false
 
 echo "Configuring Finder..."
@@ -55,4 +67,3 @@ killall System\ Preferences
 echo
 echo "Configuration is done. Please restart system."
 echo
-
