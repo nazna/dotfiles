595308a471988b79ddeb49b1784febe46fa82fe1
Naoya Yamashita <naoya3e@users.noreply.github.com>
2019-10-06 16:44:47 +0900

    chore: change bootstraping flow, assume that this script is executed only once

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -2,24 +2,12 @@
 
 set -eu
 
-DOTPATH=$HOME/workspace/ghq/github.com/naoya3e/dotfiles
-
 echo "Bootstrapping..."
 
-if [ ! -d "$DOTPATH" ]; then
-  git clone https://github.com/naoya3e/dotfiles.git $DOTPATH
-else
-  echo "$DOTPATH already exist. Updateing..."
-  cd "$DOTPATH"
-  git stash --include-untracked
-  git checkout master
-  git pull --rebase --prune origin master
-fi
-
-cd "$DOTPATH"
-
-scripts/configure.sh
-echo
+curl -L https://github.com/naoya3e/dotfiles/archive/master.zip -o dotfiles.zip
+unzip dotfiles.zip
+rm dotfiles.zip
+cd dotfiles-master
 
 if ! command -v brew >/dev/null 2>&1; then
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
@@ -29,8 +17,19 @@ fi
 brew bundle
 echo
 
+scripts/configure.sh
+echo
+
+scripts/initialize.sh
+echo
+
 scripts/deploy.sh
 echo
 
-echo "All green, Done :)"
-
+echo "All automatic configuration was done :)"
+echo
+echo "Remove this directory for bootstraping. True dotfiles are deployed."
+echo "Run the following command `cd $HOME && rm -rf dotfiles-master`"
+echo
+echo "Please setup the rest of the System Preferences while checking Dropbox Paper."
+echo "Next, setup Launcher, Finder, f.lux, Amphetamine and VSCode."
