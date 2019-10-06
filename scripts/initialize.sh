64dcf5a2f47d7dcfd4e8a64eaaa778e8f0bff3b2
Naoya Yamashita <naoya3e@users.noreply.github.com>
2019-10-06 16:21:18 +0900

    chore: add prepare process

--- scripts/initialize.sh
+++ scripts/initialize.sh
@@ -8,12 +8,17 @@ mkdir -p $HOME/.config/nvim
 
 rm $HOME/Applications/.localized $HOME/Desktop/.localized $HOME/Documents/.localized $HOME/Downloads/.localized $HOME/Library/.localized $HOME/Movies/.localized $HOME/Music/.localized $HOME/Pictures/.localized $HOME/Public/.localized
 
+echo "Prepare neovim and neovim plugins..."
 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
 sh ./installer.sh $HOME/.cache/dein
 rm ./installer.sh
 
+echo "Prepare deploying shell configuration..."
 sudo sh -c "echo '/usr/local/bin/zsh' >> /etc/shells"
 chsh -s /usr/local/bin/zsh
+
+echo "Prepare VSCode setting sync..."
+code --install-extension Shan.code-settings-sync
 
 echo "Generating ssh key..."
 ssh-keygen -t ed25519
