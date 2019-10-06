4e2cf9a7b549fcd84b3c8f827cac7f3f43d015ef
Naoya Yamashita <naoya3e@users.noreply.github.com>
2019-10-07 02:18:31 +0900

    chore: add symbolic link for starship

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,6 +9,6 @@ ln -nfs $HOME/workspace/ghq/github.com/naoya3e/dotfiles/hyper.js $HOME/.hyper.js
 ln -nfs $HOME/workspace/ghq/github.com/naoya3e/dotfiles/latexmkrc $HOME/.latexmkrc
 ln -nfs $HOME/workspace/ghq/github.com/naoya3e/dotfiles/npmrc $HOME/.npmrc
 ln -nfs $HOME/workspace/ghq/github.com/naoya3e/dotfiles/nvimrc $HOME/.config/nvim/init.vim
+ln -nfs $HOME/workspace/ghq/github.com/naoya3e/dotfiles/starship.toml $HOME/.config/starship.toml
 ln -nfs $HOME/workspace/ghq/github.com/naoya3e/dotfiles/zshenv $HOME/.zshenv
 ln -nfs $HOME/workspace/ghq/github.com/naoya3e/dotfiles/zshrc $HOME/.zshrc
-ln -nfs $HOME/workspace/ghq/github.com/naoya3e/dotfiles/editorconfig $HOME/.editorconfig
