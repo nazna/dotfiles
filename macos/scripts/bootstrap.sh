c48e98180e5f135414349e49fb3505ead5a3290f
Naoya Yamashita <naoya3e@users.noreply.github.com>
2020-04-25 16:51:47 +0900

    chore: update for xcode-select already installed

--- macos/scripts/bootstrap.sh
+++ macos/scripts/bootstrap.sh
@@ -7,18 +7,18 @@ if [ -e "$HOME/workspace/ghq/github.com/naoya3e/dotfiles" ]; then
   exit 1
 fi
 
-echo ">>> Install Xcode components"
-xcode-select --install
+if ! builtin command -v xcode-select --print-path > /dev/null; then
+  echo ">>> Install Xcode components"
+  xcode-select --install
+fi
 
 echo ">>> Fetch dotfiles"
 mkdir -p $HOME/workspace/ghq/github.com/naoya3e
 git clone https://github.com/naoya3e/dotfiles.git $HOME/workspace/ghq/github.com/naoya3e/dotfiles
 
-
-echo ">>> Install Homebrew"
 if ! builtin command -v brew > /dev/null; then
+  echo ">>> Install Homebrew"
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-  echo
 fi
 
 echo ">>> Install Homebrew Formulae"
