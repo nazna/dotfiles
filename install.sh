3e61435af76d263ded6192004342e8e891dd88b7
Naoya Yamashita <14846330+nazna@users.noreply.github.com>
2026-06-21 18:02:22 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -20,10 +20,12 @@ fi
 # Install system packages
 sudo apt update -y
 sudo apt upgrade -y
-sudo apt install -y build-essential language-pack-ja
+sudo apt install -y build-essential language-pack-ja bubblewrap
 
 # Install Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv bash)"
+cat "${DOTFILES}/misc/Brewfile" | brew bundle --file=-
 
 source ./scripts/link-dotfiles.sh
 source ./scripts/install-zsh-plugins.sh
